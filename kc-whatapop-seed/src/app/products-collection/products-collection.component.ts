c4cb13ad7a18a45ddc4bb920e0c791e174214421
Miguel Jardón <migueljp83@gmail.com>
2017-06-23 22:23:36 +0200

    Practica - Green Path DONE

---
+++ kc-whatapop-seed/src/app/products-collection/products-collection.component.ts
@@ -0,0 +1,55 @@
+import { Product } from './../product';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/switchMap';
+import { Router } from '@angular/router';
+
+import { ProductFilter } from '../product-filter';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-products-collection',
+  templateUrl: './products-collection.component.html',
+  styleUrls: ['./products-collection.component.css']
+})
+export class ProductsCollectionComponent implements OnDestroy, OnInit {
+
+  products: Product[];
+  private _filterStream$: Subject<ProductFilter> = new Subject;
+
+  constructor(
+    private _productService: ProductService,
+    private _router: Router
+    ) { }
+
+  ngOnInit(): void {
+    this._filterStream$
+      .switchMap((filter: ProductFilter) => this._productService.getProducts(filter))
+      .subscribe((products: Product[]) => this.products = products);
+    this.filterCollection(null);
+  }
+
+  ngOnDestroy(): void {
+    this._filterStream$.unsubscribe();
+  }
+
+  filterCollection(filter: ProductFilter): void {
+    this._filterStream$.next(filter);
+  }
+
+  /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+  | Green Path                                                       |
+  |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+  | Maneja el evento del componente ProductComponent que indica la   |
+  | selección de un producto y navega a la dirección correspondiente.|
+  | Recuerda que para hacer esto necesitas inyectar como dependencia |
+  | el Router de la app. La ruta a navegar es '/products', pasando   |
+  | como parámetro el identificador del producto.                    |
+  |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+  mostrarProducto(producto) {
+    // Cambios
+    this._router.navigate(['products', producto.id]);
+  }
+
+}
